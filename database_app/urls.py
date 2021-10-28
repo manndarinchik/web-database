df0137155e3f88dd9b3c5b61fed391daaf9fcdbc
Eldybyhtyn <name66437@gmail.com>
2021-10-28 14:44:17 +0300

    table_born

--- database_app/urls.py
+++ database_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from database_app import views
+from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.new, name = "index"),
 ]
