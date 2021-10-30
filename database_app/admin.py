e9eed4835327b4bd65b51c34eb3e4eb9ecc8548f
Manndarinchik <belikovgrisha9@gmail.com>
2021-10-30 14:43:17 +0300

    Created database node for storing data

--- database_app/admin.py
+++ database_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import DataNode
+
+admin.site.register(DataNode)
+
 # Register your models here.
--- database_app/models.py
+++ database_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DataNode(models.Model):
+    def __str__(self):
+        # отображение нода в консоли и утилитах  
+        return "At {}:{} - {}".format(self.row_pos, self.column_pos, self.data)
+
+    data = models.CharField(max_length=200)
+    row_pos = models.IntegerField(default=0)
+    column_pos = models.IntegerField(default=0)
