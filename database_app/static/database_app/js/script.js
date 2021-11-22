2cf340a3f0bd592c49ca82b40e0238461026c428
Ilya Kalugin <name66437@gmail.com>
2021-11-22 21:14:23 +0300

    Кнопки удаления столбцов и отмены изменений созданы (один стиль на все)

--- database_app/static/database_app/js/script.js
+++ database_app/static/database_app/js/script.js
@@ -1,34 +1,59 @@
-// Создание кнопки для добавления столбца
-let allTr = document.querySelectorAll("tr");
-const buttonColumn = document.createElement('td');
-buttonColumn.classList.add("buttonColumn");
-buttonColumn.style.visibility = 'hidden';
-allTr[0].append(buttonColumn);
+/*-------Изначальная таблица-----------*/
+const oldTr = document.querySelectorAll("tr");
+const oldTd = oldTr[0].querySelectorAll("td");
 /*-------------------------------------*/
-const agent = document.querySelector('.agent');
-agent.setAttribute('value', allTr[0].querySelectorAll('td').length - 1);
 
-buttonColumn.addEventListener('click', function() {
+
+/*Создание "агента", который передаёт информацию о кол-ве столбцов*/
+const agent = document.querySelector('.agent');
+agent.setAttribute('value', document.querySelectorAll("tr")[0].querySelectorAll('td').length);
+/*-------------------------------------*/
+
+
+/*---------Добавление колонки----------*/
+const addColumn = document.getElementById("addColumn");
+addColumn.addEventListener('click', function() {
     let allTr = document.querySelectorAll("tr");
     for (let i = 0; i < allTr.length; i++) {
-        let newTd = document.createElement('td');
-        let input = document.createElement('input');
+        newColumn = document.createElement('td');
+        newColumn.style.width = '200px';
+        let input = document.createElement("input");
         input.setAttribute('name', 'ourInput');
-        newTd.append(input);
-        if (i == 0) {
-            let allTd = allTr[i].querySelectorAll('td');
-            allTd[allTd.length - 2].after(newTd);
-            agent.setAttribute('value', allTr[0].querySelectorAll('td').length - 1);
-        } else {
-            allTr[i].append(newTd);
-        }
-
+        newColumn.append(input);
+        allTr[i].append(newColumn);
     }
+    agent.setAttribute('value', allTr[0].querySelectorAll('td').length);
 });
+/*-------------------------------------*/
 
 
-const buttonRow = document.getElementById('buttonRow');
-buttonRow.addEventListener('click', function() {
+/*----------Удаление колонки-----------*/
+const deleteColumn = document.getElementById("deleteColumn");
+deleteColumn.addEventListener('click', function() {
+    let allTr = document.querySelectorAll("tr");
+    for (let i = 0; i < allTr.length; i++) {
+        let lastTd = allTr[i].querySelectorAll("td")[allTr[i].querySelectorAll("td").length - 1];
+        agent.setAttribute('value', allTr[0].querySelectorAll('td').length);
+
+        lastTd.remove();
+    }
+    agent.setAttribute('value', allTr[0].querySelectorAll('td').length);
+});
+/*-------------------------------------*/
+
+
+/*----------Удаление строки-----------*/
+const deleteRow = document.getElementById("deleteRow");
+deleteRow.addEventListener('click', function() {
+    let lastTr = document.querySelectorAll('tr')[document.querySelectorAll("tr").length - 1];
+    lastTr.remove();
+});
+/*-------------------------------------*/
+
+
+/*----------Добавление строки----------*/
+const addRow = document.getElementById('addRow');
+addRow.addEventListener('click', function() {
     let newTr = document.createElement('tr');
     let lastTr = document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1];
     lastTr.after(newTr);
@@ -40,81 +65,85 @@ buttonRow.addEventListener('click', function() {
         newTr.append(newTd);
     }
 });
+/*-------------------------------------*/
 
-buttonColumn.addEventListener('click', function() {
 
-})
-
+/*----Режим редактирования/просмотра----*/
 const buttonChange = document.getElementById("button_change");
 let access = 0;
-
 buttonChange.addEventListener('click', function() {
-    buttonRow.style.display = "inline-block";
-    butt.style.display = "inline-block";
-
     if (access % 2 == 0) {
-        buttonRow.style.display = "inline-block";
-        butt.style.display = "inline-block";
-        buttonChange.innerHTML = "Отмена"
-
+        buttonChange.innerHTML = "Выйти"
+        document.querySelector(".buttonsColumn").style.display = 'block';
+        document.querySelector(".buttonsRow").style.display = 'block';
+        document.getElementById("justButton").style.display = 'block';
+        document.getElementById("resetButton").style.display = 'block';
         access += 1;
-        buttonColumn.style.visibility = 'visible';
-        buttonRow.style.display = 'block';
         let allTr = document.querySelectorAll('tr');
         for (let i = 0; i < allTr.length; i++) {
             let allTd = allTr[i].querySelectorAll('td');
             for (let j = 0; j < allTd.length; j++) {
                 let tdText = String(allTd[j].innerHTML).trim();
-                if (allTd[j].className != 'buttonColumn') {
-                    allTd[j].innerHTML = '';
-                    let tempInput = document.createElement('input');
-                    tempInput.setAttribute('name', 'ourInput');
-                    tempInput.value = tdText;
-                    allTd[j].append(tempInput);
-                }
+                allTd[j].innerHTML = '';
+                let tempInput = document.createElement('input');
+                tempInput.setAttribute('name', 'ourInput');
+                tempInput.value = tdText;
+                allTd[j].append(tempInput);     
             }
         }
-
     } else {
-        buttonRow.style.display = "none";
-        butt.style.display = "none";
+        document.querySelector(".buttonsColumn").style.display = 'none';
+        document.querySelector(".buttonsRow").style.display = 'none';
+        document.getElementById("justButton").style.display = 'none';
+        document.getElementById("resetButton").style.display = 'none';
         buttonChange.innerHTML = "Изменить"
-
         access -= 1;
-        buttonColumn.style.visibility = 'hidden';
-        buttonRow.style.display = 'none';
         let allTr = document.querySelectorAll('tr');
         for (let i = 0; i < allTr.length; i++) {
             let allTd = allTr[i].querySelectorAll('td');
             for (let j = 0; j < allTd.length; j++) {
-                if (allTd[j].className != 'buttonColumn') {
-                    let tdText = String(allTd[j].childNodes[0].value).trim();
-                    allTd[j].childNodes[0].remove();
-                    allTd[j].innerHTML = tdText;
-                }
+                let tdText = String(allTd[j].childNodes[0].value).trim();
+                allTd[j].childNodes[0].remove();
+                allTd[j].innerHTML = tdText;
             }
         }
     }
 });
+/*-------------------------------------*/
 
-let butt = document.getElementById('justButton');
+
+/*-----------Кнопка "отмена"-----------*/
+
+let resetButton = document.getElementById("resetButton");
+resetButton.addEventListener('click', function() {
+    let newTr = document.querySelectorAll("tr");
+    if (newTr.length > oldTr.length) {
+        for (let i = oldTr.length; i < newTr.length; i++) {
+            newTr[i].remove();
+        }
+    }
+    for (let i = 0; i < oldTr.length; i++) {
+        let newTd = newTr[i].querySelectorAll("td");
+        for (let j = oldTd.length; j < newTd.length; j++) {
+            newTd[j].remove();  
+        }
+        
+    }
+    agent.setAttribute('value', oldTr[0].querySelectorAll('td').length);
+});
+
+/*-------------------------------------*/
 
 
-butt.addEventListener('submit', function(e) {
+/*-----------Отправка данных-----------*/
+let submitButton = document.getElementById('justButton');
+submitButton.addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    //let csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-    const newForm = new FormData();
-    
+    const newForm = new FormData();  
     let row = agent.value;
-
     newForm.append(row)
-
-    
     const xhr = new XMLHttpRequest();
-
     xhr.open('POST', '');
     xhr.send(newForm);
-
-    
-})
+});
+/*-------------------------------------*/
